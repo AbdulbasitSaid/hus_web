236b3abb7c5783509b24b5a0cf7c4c0bcecb4421
abdulbasit <mrspeed518@gmail.com>
2020-09-19 02:08:02 +0100

    done

---
+++ app.js
@@ -0,0 +1,17 @@
+
+
+function showNav() {
+    document.getElementById('sidebar').style.width = '200px';
+    document.getElementById('sidebar').style.opacity = '1';
+
+    document.getElementById('sidebar').style.visibility = 'visible';
+
+}
+function hideNav() {
+    document.getElementById('sidebar').style.width = '0px';
+    document.getElementById('sidebar').style.opacity = '0';
+    document.getElementById('sidebar').style.visibility = 'invisible';
+
+}
+
+
